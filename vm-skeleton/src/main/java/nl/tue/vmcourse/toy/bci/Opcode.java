7747ce29724f01ff351e76d8236ae04ca8bf1492
Kristiyan Michaylov <krisidm24@gmail.com>
2024-09-18 13:37:19 +0200

    Add byte values to enum for better performance

--- vm-skeleton/src/main/java/nl/tue/vmcourse/toy/bci/Opcode.java
+++ vm-skeleton/src/main/java/nl/tue/vmcourse/toy/bci/Opcode.java
@@ -1,28 +1,38 @@
 package nl.tue.vmcourse.toy.bci;
 
 public enum Opcode {
-    OP_LOAD,
-    OP_RETURN,
-    OP_PRINT,
-    OP_LOOP,
-    OP_JUMP,
-    OP_POP,
-    OP_PUSH,
+    OP_LOAD((byte) 0x01),
+    OP_RETURN((byte) 0x02),
+    OP_PRINT((byte) 0x03),
+    OP_LOOP((byte) 0x04),
+    OP_JUMP((byte) 0x05),
+    OP_POP((byte) 0x06),
+    OP_PUSH((byte) 0x07),
     // Comparisons
-    OP_NOT,
-    OP_NOT_EQUAL,
-    OP_EQUAL,
-    OP_GREATER,
-    OP_LESS,
+    OP_NOT((byte) 0x08),
+    OP_NOT_EQUAL((byte) 0x09),
+    OP_EQUAL((byte) 0x0A),
+    OP_GREATER((byte) 0x0B),
+    OP_LESS((byte) 0x0C),
     // Binary values
-    OP_NULL,
-    OP_TRUE,
-    OP_FALSE,
+    OP_NULL((byte) 0x0D),
+    OP_TRUE((byte) 0x0E),
+    OP_FALSE((byte) 0x0F),
     // Binary operations
-    OP_ADD,
-    OP_SUB,
-    OP_MUL,
-    OP_DIV,
+    OP_ADD((byte) 0x10),
+    OP_SUB((byte) 0x11),
+    OP_MUL((byte) 0x12),
+    OP_DIV((byte) 0x13),
     // Literals
-    OP_CONSTANT,
+    OP_CONSTANT((byte) 0x14);
+
+    private final byte value;
+
+    Opcode(byte value) {
+        this.value = value;
+    }
+
+    public byte getValue() {
+        return value;
+    }
 }
