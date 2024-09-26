47ff9dac02a2d6d5c863db6699bd6855880fc5e9
Kristiyan Michaylov <krisidm24@gmail.com>
2024-09-26 23:15:55 +0200

    Remove unused imports

--- vm-skeleton/src/main/java/nl/tue/vmcourse/toy/bci/Instruction.java
+++ vm-skeleton/src/main/java/nl/tue/vmcourse/toy/bci/Instruction.java
@@ -1,7 +1,5 @@
 package nl.tue.vmcourse.toy.bci;
 
-import java.util.Optional;
-
 /**
  * The instruction record is essentially a wrapper over the opcode which helps to store information about the operands.
  **/
