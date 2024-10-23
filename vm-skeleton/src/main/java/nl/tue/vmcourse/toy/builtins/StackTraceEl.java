998ad8995dcc65068a7b26cdd182e5f4177d25f9
Kristiyan Michaylov <k.d.michaylov@student.tue.nl>
2024-10-23 11:16:33 +0200

    Minor

--- vm-skeleton/src/main/java/nl/tue/vmcourse/toy/builtins/StackTraceEl.java
+++ vm-skeleton/src/main/java/nl/tue/vmcourse/toy/builtins/StackTraceEl.java
@@ -1,7 +1,6 @@
 package nl.tue.vmcourse.toy.builtins;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.AbstractMap.SimpleEntry;
@@ -25,7 +24,6 @@ public class StackTraceEl {
                 return;
             }
         }
-        // If the key does not exist, add a new entry
         addElement(key, value);
     }
 
