580e0542f86e8b697de57bfd95795bc62959df70
Kristiyan Michaylov <k.d.michaylov@student.tue.nl>
2024-10-23 23:52:19 +0200

    Initial String rope classes [currently passing 235 tests]

---
+++ vm-skeleton/src/main/java/nl/tue/vmcourse/toy/optimization/Node.java
@@ -0,0 +1,12 @@
+package nl.tue.vmcourse.toy.optimization;
+
+/**
+ * Just the top level class for the ropes optimization.
+ */
+public class Node {
+    int weight;
+
+    public int length() {
+        return weight;
+    }
+}
---
+++ vm-skeleton/src/main/java/nl/tue/vmcourse/toy/optimization/ParentNode.java
@@ -0,0 +1,12 @@
+package nl.tue.vmcourse.toy.optimization;
+
+public class ParentNode extends Node {
+        Node left;
+        Node right;
+
+        public ParentNode(Node left, Node right) {
+            this.left = left;
+            this.right = right;
+            this.weight = left.length();  // Internal node's weight is the length of the left subtree
+        }
+}
