7d5b1d2ae8e6da409bee39aec06ae7a1a97fbb04
Kristiyan Michaylov <k.d.michaylov@student.tue.nl>
2024-10-25 21:12:20 +0200

    Fix argument problem [currently passing 235 tests]

--- vm-skeleton/src/main/java/nl/tue/vmcourse/toy/ToyLauncher.java
+++ vm-skeleton/src/main/java/nl/tue/vmcourse/toy/ToyLauncher.java
@@ -49,106 +49,106 @@ public class ToyLauncher {
     public static boolean ROPES_ENABLED = false;
     public static boolean ARRAYS_ENABLED = false;
 
-    private static boolean isOptimizationSupported(String arg) {
-        boolean optimizationSupported = true;
+    private static boolean isOptimizationSupported(String[] args) {
+        boolean optimizationSupported = false;
 
-        switch (arg) {
-            case "-jit":
-                JIT_ENABLED = true;
-                return false;
-            case "-inline-caches":
-                IC_ENABLED = true;
-                return false;
-            case "-string-ropes":
+        for (String arg : args) {
+            if (arg.equals("-jit")) {
+                System.out.println("Optimization not supported");
+                System.exit(1);
+            }
+            if (arg.equals("-string-ropes")) {
                 ROPES_ENABLED = true;
-                return true;
-            case "-array-strategies":
-                ARRAYS_ENABLED = true;
-                return false;
+                optimizationSupported = true;
+                break;
+            }
+//            if (arg.equals("-inline-caches")) {
+//                IC_ENABLED = true;
+//                optimizationSupported = true;
+//            }
+//            if (arg.equals("-array-strategies")) {
+//                ARRAYS_ENABLED = true;
+//                optimizationSupported = true;
+//            }
         }
         return optimizationSupported;
     }
 
-    public static Object eval(String code) {
-        return evalStream(CharStreams.fromString(code));
-    }
+public static Object eval(String code) {
+    return evalStream(CharStreams.fromString(code));
+}
 
-    public static String parseReportErrors(String code) {
-        CharStream charStream = CharStreams.fromString(code);
-        String src = charStream.getText(Interval.of(0, charStream.size()));
-        ToyLangLexer lex = new ToyLangLexer(charStream);
-        CommonTokenStream tokens = new CommonTokenStream(lex);
-        ToyLangParser parser = new ToyLangParser(tokens);
-        ToyNodeFactory factory = new ToyNodeFactory(src);
-        parser.setFactory(factory);
-        lex.removeErrorListener(ConsoleErrorListener.INSTANCE);
-        parser.removeErrorListener(ConsoleErrorListener.INSTANCE);
-        lex.addErrorListener(new ToyLangParser.BailoutErrorListener());
-        parser.addErrorListener(new ToyLangParser.BailoutErrorListener());
-        try {
-            parser.toylanguage();
-        } catch (RuntimeException e) {
-            return e.getMessage();
-        }
-        return null;
-    }
-
-    private static Object evalStream(CharStream charStream) {
-        String src = charStream.getText(Interval.of(0, charStream.size()));
-        ToyLangLexer lex = new ToyLangLexer(charStream);
-        CommonTokenStream tokens = new CommonTokenStream(lex);
-        ToyLangParser parser = new ToyLangParser(tokens);
-        ToyNodeFactory factory = new ToyNodeFactory(src);
-        parser.setFactory(factory);
-        lex.removeErrorListeners();
-        parser.removeErrorListeners();
-        lex.addErrorListener(new ToyLangParser.BailoutErrorListener());
-        parser.addErrorListener(new ToyLangParser.BailoutErrorListener());
+public static String parseReportErrors(String code) {
+    CharStream charStream = CharStreams.fromString(code);
+    String src = charStream.getText(Interval.of(0, charStream.size()));
+    ToyLangLexer lex = new ToyLangLexer(charStream);
+    CommonTokenStream tokens = new CommonTokenStream(lex);
+    ToyLangParser parser = new ToyLangParser(tokens);
+    ToyNodeFactory factory = new ToyNodeFactory(src);
+    parser.setFactory(factory);
+    lex.removeErrorListener(ConsoleErrorListener.INSTANCE);
+    parser.removeErrorListener(ConsoleErrorListener.INSTANCE);
+    lex.addErrorListener(new ToyLangParser.BailoutErrorListener());
+    parser.addErrorListener(new ToyLangParser.BailoutErrorListener());
+    try {
         parser.toylanguage();
-
-        Map<String, RootCallTarget> allFunctions = factory.getAllFunctions();
-        if (!allFunctions.isEmpty() && allFunctions.containsKey("main")) {
-            RootCallTarget mainFunction = allFunctions.get("main");
-            // TODO register builtins, initialize global scope, ...
-            // TODO ME: Maybe check if here you can do the buildins or leave them as they are.
-            // TODO ME: Basically, we should always start with the main function, see how to be able to check other functions.
-
-            for (Map.Entry<String, RootCallTarget> entry : allFunctions.entrySet()) {
-                // here, we add the global scope for functions and then create the bytecode for each function.
-                globalScope.registerFunction(entry.getKey(), entry.getValue());
-                globalScope.setFunctionToNumberOfArguments(entry.getKey(), factory.getFunctionParameterCount(entry.getKey()));
-            }
-            return mainFunction.invoke(globalScope);
-        } else {
-            System.err.println("No function main() defined in SL source file.");
-            System.exit(1);
-        }
-        return null;
+    } catch (RuntimeException e) {
+        return e.getMessage();
     }
+    return null;
+}
 
-    public static void main(String[] args) throws IOException {
-        // TODO: change this when you will need to provide more arguments
-        if (args.length < 1) {
-            System.out.println("Usage: toy [file]");
-            System.exit(1);
-        }
-//TODO: Uncomment this when you will need to provide more arguments
-//        boolean optimizationSupported = isOptimizationSupported(args[args.length - 2]);
-//
-//        if (!optimizationSupported) {
-//            System.out.println("Optimization not supported");
-//            System.exit(1);
-//        }
+private static Object evalStream(CharStream charStream) {
+    String src = charStream.getText(Interval.of(0, charStream.size()));
+    ToyLangLexer lex = new ToyLangLexer(charStream);
+    CommonTokenStream tokens = new CommonTokenStream(lex);
+    ToyLangParser parser = new ToyLangParser(tokens);
+    ToyNodeFactory factory = new ToyNodeFactory(src);
+    parser.setFactory(factory);
+    lex.removeErrorListeners();
+    parser.removeErrorListeners();
+    lex.addErrorListener(new ToyLangParser.BailoutErrorListener());
+    parser.addErrorListener(new ToyLangParser.BailoutErrorListener());
+    parser.toylanguage();
 
-        // TODO, ignores other args for now.
-        CharStream charStream = CharStreams.fromFileName(args[args.length - 1]);
-        try {
-            Object result = evalStream(charStream);
-            if (result != null)
-                System.out.println(result);
-        } catch (ToySyntaxErrorException e) {
-            System.err.println("Error(s) parsing script :(");
-            System.exit(1);
+    Map<String, RootCallTarget> allFunctions = factory.getAllFunctions();
+    if (!allFunctions.isEmpty() && allFunctions.containsKey("main")) {
+        RootCallTarget mainFunction = allFunctions.get("main");
+        // TODO register builtins, initialize global scope, ...
+        // TODO ME: Maybe check if here you can do the buildins or leave them as they are.
+        // TODO ME: Basically, we should always start with the main function, see how to be able to check other functions.
+
+        for (Map.Entry<String, RootCallTarget> entry : allFunctions.entrySet()) {
+            // here, we add the global scope for functions and then create the bytecode for each function.
+            globalScope.registerFunction(entry.getKey(), entry.getValue());
+            globalScope.setFunctionToNumberOfArguments(entry.getKey(), factory.getFunctionParameterCount(entry.getKey()));
         }
+        return mainFunction.invoke(globalScope);
+    } else {
+        System.err.println("No function main() defined in SL source file.");
+        System.exit(1);
     }
+    return null;
+}
+
+public static void main(String[] args) throws IOException {
+    // TODO: change this when you will need to provide more arguments
+    if (args.length < 1) {
+        System.out.println("Usage: toy [file]");
+        System.exit(1);
+    }
+    isOptimizationSupported(args);
+
+
+    // TODO, ignores other args for now.
+    CharStream charStream = CharStreams.fromFileName(args[args.length - 1]);
+    try {
+        Object result = evalStream(charStream);
+        if (result != null)
+            System.out.println(result);
+    } catch (ToySyntaxErrorException e) {
+        System.err.println("Error(s) parsing script :(");
+        System.exit(1);
+    }
+}
 }
