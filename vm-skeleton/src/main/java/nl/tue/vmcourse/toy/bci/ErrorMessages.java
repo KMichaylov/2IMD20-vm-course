f4d8edeeb1671b9836186fe8bcd3198f7e7cda61
Kristiyan Michaylov <k.d.michaylov@student.tue.nl>
2024-11-02 22:20:00 +0100

    Add documentation for GlobalScope and ErrorMessages [currently passing 248 tests]

--- vm-skeleton/src/main/java/nl/tue/vmcourse/toy/bci/ErrorMessages.java
+++ vm-skeleton/src/main/java/nl/tue/vmcourse/toy/bci/ErrorMessages.java
@@ -2,8 +2,17 @@ package nl.tue.vmcourse.toy.bci;
 
 import java.util.Map;
 
+/**
+ * The following class is created to add in reporting error messages for the undefined or type errors.
+ */
 public class ErrorMessages {
 
+    /**
+     * The following method returns the type of the given object.
+     *
+     * @param obj which will be checked
+     * @return the type of the object
+     */
     private String returnTypeOfObject(Object obj) {
         if (obj == null || obj.equals("NULL")) {
             return null;
@@ -13,24 +22,37 @@ public class ErrorMessages {
             return "String";
         } else if (obj instanceof Boolean) {
             return "Boolean";
-        } else if (obj instanceof Object){
+        } else if (obj instanceof Object) {
             return "Object";
-        }else {
+        } else {
             return obj.getClass().getSimpleName();
         }
     }
 
+    /**
+     * The following method simply returns the value of the object.
+     *
+     * @param obj for which value will be returned
+     * @return the value of the object
+     */
     private String returnValueOfObject(Object obj) {
         if (obj instanceof String && !obj.equals("NULL")) {
             return "\"" + obj + "\"";
-        } else if (obj instanceof Map){
+        } else if (obj instanceof Map) {
             return "Object";
-        }
-        else {
+        } else {
             return obj.toString();
         }
     }
 
+    /**
+     * Generates a type error for the given 2 variables and operation.
+     *
+     * @param left      the left operand
+     * @param right     the right operand
+     * @param operation the operation(+, -, *, /)
+     * @return the generated error message
+     */
     public String generateTypeError(Object left, Object right, String operation) {
         String leftType = left == null ? "" : returnTypeOfObject(left);
         String rightType = right == null ? "" : returnTypeOfObject(right);
@@ -40,29 +62,65 @@ public class ErrorMessages {
         return message.replaceAll(" {2}", " ");
     }
 
+    /**
+     * Generates a type error for the object and operation.
+     *
+     * @param obj       the object
+     * @param operation the operation
+     * @return the generated error message
+     */
     public String generateTypeError(Object obj, String operation) {
         String objType = returnTypeOfObject(obj);
         return "Type error: operation \"" + operation + "\" not defined for " + objType + " \"" + obj + "\"\n";
     }
 
+    /**
+     * Generates a type error for the given operation.
+     *
+     * @param operation the operation(+, -, *, /)
+     * @return the generated error message
+     */
     public String generateTypeErrorNull(String operation) {
         return "Type error: operation \"" + operation + "\" not defined for " + "NULL" + ", " + "NULL" + "\n";
     }
 
+    /**
+     * Generates a type error for the object and operation for booleans.
+     *
+     * @param obj       the object
+     * @param operation the operation
+     * @return the generated error message
+     */
     public String generateBooleanTypeError(Object obj, String operation) {
         String objType = returnTypeOfObject(obj);
         return "Type error: operation \"" + operation + "\" not defined for " + objType + " \"" + obj + "\", ANY\n";
     }
 
+    /**
+     * Generate a type error for the given function and argument.
+     * @param functionName the name of the function
+     * @param argument the argument of the function
+     * @return the generated error message
+     */
     public String generateTypeErrorForDefineFunction(String functionName, Object argument) {
         String objType = returnTypeOfObject(argument);
         return "Type error: operation \"" + functionName + "\" not defined for " + objType + " " + argument + "\n";
     }
 
+    /**
+     * Simply generates an undefined function error message.
+     * @param functionName name of the function
+     * @return the generated error message
+     */
     public String generateUndefinedFunction(String functionName) {
         return "Undefined function: " + functionName + "\n";
     }
 
+    /**
+     * Simply generates an error message for the undefined object property.
+     * @param propertyName of the object
+     * @return the generated error message
+     */
     public String generateUndefinedObjectProperty(String propertyName) {
         return "Undefined property: " + propertyName + "\n";
     }
--- vm-skeleton/src/main/java/nl/tue/vmcourse/toy/bci/GlobalScope.java
+++ vm-skeleton/src/main/java/nl/tue/vmcourse/toy/bci/GlobalScope.java
@@ -6,35 +6,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * The following class is used to store the global scope of the project, namely functions, some variables, so
+ * The following class is used to store the global scope of the project, namely functions so
  * that we are aware how to locate them. It uses hashmaps, since this is a simple way to fetch the necessary data.
  */
 public class GlobalScope {
-    private final Map<String, Object> variables = new HashMap<>();
     private final Map<String, RootCallTarget> functions = new HashMap<>();
     private final Map<String, Integer> functionToNumberOfArguments = new HashMap<>();
 
     /**
-     * Just sets the variable name to the object in a hashmap.
-     * @param name of the variable
-     * @param value the value of the variable
-     */
-    public void setVariable(String name, Object value) {
-        variables.put(name, value);
-    }
-
-    /**
-     * Gets the variable from the hashmap.
-     * @param name of the variable
-     * @return the value of the variable
-     */
-    public Object getVariable(String name) {
-        return variables.get(name);
-    }
-
-    /**
      * Registers a function for the whole program.
-     * @param name  of the function
+     *
+     * @param name     of the function
      * @param function the function object which is used for execution
      */
     public void registerFunction(String name, RootCallTarget function) {
@@ -43,6 +25,7 @@ public class GlobalScope {
 
     /**
      * Gets the function from the hashmap.
+     *
      * @param name of the function
      * @return the function object
      */
@@ -51,18 +34,9 @@ public class GlobalScope {
     }
 
     /**
-     * Increases the number of arguments for the specific function.
-     * @param name of the function
-     */
-    public void increaseFunctionToNumberOfArguments(String name) {
-        int currentNumberOfArguments = functionToNumberOfArguments.getOrDefault(name, 0);
-        currentNumberOfArguments++;
-        functionToNumberOfArguments.replace(name, currentNumberOfArguments);
-    }
-
-    /**
      * Directly sets the number of arguments for the specific function.
-     * @param name of the function
+     *
+     * @param name              of the function
      * @param numberOfArguments the function has
      */
     public void setFunctionToNumberOfArguments(String name, Integer numberOfArguments) {
@@ -71,6 +45,7 @@ public class GlobalScope {
 
     /**
      * Juts return all functions from the global scope.
+     *
      * @return functions as a hashmap
      */
     public Map<String, RootCallTarget> getAllFunctions() {
@@ -79,6 +54,7 @@ public class GlobalScope {
 
     /**
      * Gets the number of arguments for the specific function.
+     *
      * @param name of the function
      * @return the number of arguments as integer value
      */
