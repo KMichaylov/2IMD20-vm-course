bba8ec047b8037150665e6c50da87826045dbeea
Kristiyan Michaylov <k.d.michaylov@student.tue.nl>
2024-11-08 21:13:38 +0100

    Document the StringRopes class [currently passing 248 tests without optimization]

--- vm-skeleton/src/main/java/nl/tue/vmcourse/toy/optimization/ropes/StringRopes.java
+++ vm-skeleton/src/main/java/nl/tue/vmcourse/toy/optimization/ropes/StringRopes.java
@@ -5,19 +5,40 @@ package nl.tue.vmcourse.toy.optimization.ropes;
  */
 public class StringRopes {
     private Node root;
+
+    /**
+     * Constructor which adds the string to the top node.
+     * @param data that is a string.
+     */
     public StringRopes(String data) {
         this.root = new SingleNodeElement(data);
     }
 
-    public StringRopes concatenation(StringRopes stringRopesInstance) {
-        this.root = concatenation(this.root, stringRopesInstance.root);
+    /**
+     * Concatenates the root node with the provided node.
+     * @param otherRope the rope to concatenate with
+     * @return the update StringRopes object
+     */
+    public StringRopes concatenation(StringRopes otherRope) {
+        this.root = generateParent(this.root, otherRope.root);
         return this;
     }
 
-    private Node concatenation(Node left, Node right) {
+    /**
+     * Generates a parent node from both left and right nodes.
+     * @param left node
+     * @param right node
+     * @return parent instance generated from the two nodes
+     */
+    private Node generateParent(Node left, Node right) {
         return new ParentNode(left, right);
     }
 
+    /**
+     * Function for the string representation of the node.
+     * @param node the node for which we want the string representation
+     * @return the string representation
+     */
     private String getStringRepresentation(Node node) {
         if (node instanceof SingleNodeElement) {
             return ((SingleNodeElement) node).data;
